96385349eacf03f832e47091da543779b37a8ee9
wrakc <carlosveiga1618@gmail.com>
2020-06-29 19:03:39 -0300

    fix rubocop offenses

--- bubble.rb
+++ bubble.rb
@@ -1,33 +1,27 @@
 def bubble_sort(arr)
-  arr.each {
+  arr.each do
     (arr.length - 1).times do |index|
-      if arr[index] > arr[index + 1]
-        arr[index], arr[index + 1] = arr[index + 1], arr[index]
-      end
+      arr[index], arr[index + 1] = arr[index + 1], arr[index] if arr[index] > arr[index + 1]
     end
-  }
+  end
 end
 
 def bubble_sort_by(str_arr)
-  str_arr.each {
+  str_arr.each do
     (str_arr.length - 1).times do |index|
       res = yield str_arr[index], str_arr[index + 1]
-      if res > 0
-        str_arr[index], str_arr[index + 1] = str_arr[index + 1], str_arr[index]
-      end
+      str_arr[index], str_arr[index + 1] = str_arr[index + 1], str_arr[index] if res > 0
     end
-  }
+  end
   str_arr
 end
 
+print bubble_sort([5, 3, 5, 6, 7, 2, 0])
 
-result = bubble_sort_by(%w[hi i hello hey kazakhstan banana orange apple mango]) do |left, right|
+result = bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
 
-puts result
+puts ""
 
-puts bubble_sort([5, 3, 5, 6, 7, 2, 0])
-
-
-
+print result
