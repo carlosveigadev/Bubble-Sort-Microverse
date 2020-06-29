53ae55c4bb24bc6864fedb95f65e1eeb60b097e0
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-06-30 00:25:01 +0300

    finish the bubble_sort function

--- bubble.rb
+++ bubble.rb
@@ -1,32 +1,16 @@
 def bubble_sort(arr)
-    for i in (arr.length - 1).times do
-        arr.each_with_index do |item, index|
-            p index
-
-            if arr[index] > arr[i]
-                arr[i], arr[index] = arr[index], arr[i]
-            else
-                p arr
-                break
-            end           
-        end
+  change = 0
+  arr.each { |i|
+    (arr.length - 1).times do |index|
+      if arr[index] > arr[index + 1]
+        arr[index], arr[index + 1] = arr[index + 1], arr[index]
+        change = 1
+      end
     end
-    
+  }
 end
 
-    # while change = true do
-    #     n.times {}
-    #         item[i], item[i +1] = item[i +1], item[i]
-    #         change = true
-    # end   
-    # n = arr.lenght -1
-    # for n.times in arr do
-    #     if item[i] > item[i +1]
-    #         item[i], item[i +1] = item[i +1], item[i]
-    #     return arr
-    # end
+p bubble_sort([5, 3, 5, 6,7, 2, 0])
 
-
-print bubble_sort([2, 3, 1])
 
 
